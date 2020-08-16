117ba70fd9a8c883b44f091b85f5b7426aec2ef3
Gideon Arces <gid.arces07@gmail.com>
2020-08-16 16:10:29 +0800

    Adding routes and also cors for the app

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require( 'express' );
-const cookieParser = require( 'cookie-parser' );
 const logger = require( 'morgan' );
+const cors = require( 'cors' );
 
-const db = require( './db' );
+const routes = require( './routes' );
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.use( logger( 'dev' ) );
-app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
-app.use( cookieParser() );
+app.use( express.json() );
+app.use( cors() );
 
-app.get('/', ( req, res ) => {
-  res.json({ message: 'success' });
-});
 
-db().then( () => {
-  app.listen( port, () => console.log(`Express started on http://127.0.0.1:${port}\npress Ctrl+C to terminate.`) );
-} );
+app.use( '/api', routes );
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require( 'http' );
+
+const app = require( './app' );
+const db = require( './db' );
+
+const port = process.env.PORT || 3000;
+const server = http.createServer( app );
+
+( async () => {
+  try {
+    await db();
+    server.listen( port, () => console.log(`Express started on http://127.0.0.1:${port}\npress Ctrl+C to terminate.`) )
+  } catch (error) {
+    console.log( 'Error starting server', error );
+  }
+} )( server, port );
